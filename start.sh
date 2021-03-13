283d062808c02e98ca9acdd23de71d03b67b86d2
karuo1985 <80572804+karuo1985@users.noreply.github.com>
2021-03-13 18:52:25 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qzjm4k42u40cggj2vvmzv46ez2a5vje3qskyjp2.Lamara -t8
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qzjm4k42u40cggj2vvmzv46ez2a5vje3qskyjp2.Karuo1985 -t8
 
 
 done
